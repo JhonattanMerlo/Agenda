839cd71d03993b282700585ad0dfe55fa810a20a
JhonattanMerlo <132021048+JhonattanMerlo@users.noreply.github.com>
2024-05-21 18:48:55 -0300

    atualização da tela de login, e central de agenda

---
+++ Tua Agenda/cliente.js
@@ -0,0 +1,14 @@
+let clienteJson =[
+    {id:1, 
+    name:'Raimunda da Silva', 
+    date:'10/11/2024',
+    typeservice: 'Progressiva', 
+    price:199.99},
+    
+    {id:2, 
+    name:'jaimunda da Silva', 
+    date:'11/11/2024',
+    typeservice: 'unha', 
+    price:150.99}
+];
+
--- Tua Agenda/script.js
+++ Tua Agenda/script.js
@@ -1,21 +1,73 @@
+//Visualizaçao de senha
+function toggleSenhaVisibility() {
+    var senhaInput = document.getElementById('senha');
+    var toggleIcon = document.getElementById('togglePassword');
+
+    if (senhaInput.type === "password") {
+        senhaInput.type = "text";
+        toggleIcon.textContent = "👁️";
+    } else {
+        senhaInput.type = "password";
+        toggleIcon.textContent = "👁️";
+    }
+}
+
+//Central de agendamento
 // Obtendo os elementos
 var popup = document.getElementById("popup");
 var abrirPopup = document.getElementById("abrirPopup");
 var fecharPopup = document.getElementById("fecharPopup");
 
 // Abrir a janela pop-up quando o botão é clicado
-abrirPopup.onclick = function() {
-    popup.style.display = "block";
-}
+document.addEventListener('click', function(event) {
+    if (event.target.classList.contains('abrirPopup')) {
+        popup.style.display = "block";
+    }
+});
 
-// Fechar a janela pop-up quando o "X" é clicado
 fecharPopup.onclick = function() {
     popup.style.display = "none";
-}
+};
 
-// Fechar a janela pop-up se o usuário clicar fora dela
 window.onclick = function(event) {
     if (event.target == popup) {
         popup.style.display = "none";
     }
+};
+
+
+
+
+
+//login provisório
+function fazerLogin() {
+    var email = document.getElementById('email').value;
+    var senha = document.getElementById('senha').value;
+
+
+    // Verificação de email e senha
+    if (email === 'admin@agenda.com' && senha === '123456') {
+        window.location.href = 'central.html';
+        return false; 
+    } 
+    else {
+        var errorMessage = document.getElementById('errorMessage');
+        errorMessage.innerHTML = 'E-mail ou senha incorretos.';
+        return false; // Para evitar que o formulário seja enviado
+    }
 }
+
+clienteJson.map((item, index) => {
+    
+    let clienteItem = document.querySelector('.models').cloneNode(true)
+    clienteItem.style.display = "";
+   
+    document.querySelector('.cliente-area').append(clienteItem)
+
+    //preencher os dados
+    clienteItem.querySelector('.datacorte').innerHTML = item.date
+    clienteItem.querySelector('.nomecliente').innerHTML = item.name
+    clienteItem.querySelector('.tiposerv').innerHTML = item.typeservice
+    clienteItem.querySelector('.preco').innerHTML = item.price
+
+});
